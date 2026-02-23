8128819eae89d8250c8376af2f77ae93a075a979
agiro-tgn-ics <agiro.tgn.ics@gencat.cat>
2026-02-23 10:14:43 +0100

    API takes model from MLflow

--- services/api/app/schemas.py
+++ services/api/app/schemas.py
@@ -2,7 +2,8 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class PredictRequest(BaseModel):
-    features: List[float] = Field(..., min_length=1)
+    # Diabetes dataset = 10 features
+    features: List[float] = Field(..., min_length=10, max_length=10)
 
 class PredictResponse(BaseModel):
     prediction: float
